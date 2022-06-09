1a3afaba7a4d07a2848549cdc1f9d91f16150835
julio picazo <juliopicazo06@gmail.com>
2022-06-09 18:54:59 -0500

    creating modules

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import 'react-native-gesture-handler';
+
+import React from 'react'
+import { Text, View } from 'react-native';
+
+
+const App = () => {
+  return (
+    <View>
+      <Text>
+        Que tranza
+      </Text>
+    </View>
+  )
+}
+
+export default App;
